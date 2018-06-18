1a435d49e837249e8754a2bd1617a87088af5318
Avinash Tiwari <qwe123kids@gmail.com>
2018-06-17 22:41:58 -0600

    find software for machine learning

---
+++ machineLearining/1_finding_If-Software_are_installed/FindSoftwareInstalled.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[17]:
+
+
+from __future__ import print_function
+import IPython
+print ('IPhyton', IPython.__version__)
+
+import numpy
+print ('numpy', numpy.__version__)
+
+import scipy
+print ('scipy', scipy.__version__)
+
+import matplotlib
+print ('matplotlib', matplotlib.__version__)
+
+import sklearn
+print ('sklearn', sklearn.__version__)
+
+import seaborn
+print ('seaborn', seaborn.__version__)
+
+import pandas
+print ('pandas', pandas.__version__)
+
+import keras
+print ('keras', keras.__version__)
+
+import tensorflow
+print ('tensorflow', tensorflow.__version__)
+
