edba5484b1cd27cbf6f4d46f193d37decf47c629
Avinash Tiwari <qwe123kids@gmail.com>
2018-06-25 18:40:50 -0600

    no message

--- Java/EclpiseAINeuralNetwork/SingleNerualNetwork/src/avinash/learn/single/neural/network/Perceptron.java
+++ Java/EclpiseAINeuralNetwork/SingleNerualNetwork/src/avinash/learn/single/neural/network/Perceptron.java
@@ -17,15 +17,36 @@ public class Perceptron {
 
 	private void initializeweight() {
 		// TODO Auto-generated method stub
+		for(int i=0; i< numberOfWeights; i++){
+			weights[i] = 0;
+		}
 		
 	}
 	
 	public void train(float learningrate, int epocs){
+		float totalError = 1;
+		while(totalError != 0 ){
+			totalError = 0;
+			for(int i=0; i < output.length; i++){
+				float calcuatedoutput = calculateTheOutput(input[i]);
+				float error = Math.abs(output[i] - calcuatedoutput);
+				totalError = totalError +error;
+				for(int j =0 ; j < numberOfWeights; j++){
+					weights[j] = weights[j] +  learningrate * input[i][j] * error;
+				}
+			}
+		}
 		
+		 System.out.println("Keep on training the neural network " + totalError);
 	}
 	
-	public float calculateTheOutput(float[] output){
+	public float calculateTheOutput(float[] input){
+		float sum = 0f;
+		for(int i =0; i < input.length; ++i){
+			sum = sum + weights[i] * input[i];
+		}
 		
+		return ActivarionFunction.stepFunction(sum);
 	}
 
 }
