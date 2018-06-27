ecaf7672943a86d84a03368dff35640db2e154d3
Avinash Tiwari <qwe123kids@gmail.com>
2018-06-26 19:20:03 -0600

    no message

--- Java/EclpiseAINeuralNetwork/SingleNerualNetwork/src/avinash/learn/single/neural/network/App.java
+++ Java/EclpiseAINeuralNetwork/SingleNerualNetwork/src/avinash/learn/single/neural/network/App.java
@@ -1,5 +1,23 @@
 package avinash.learn.single.neural.network;
 
 public class App {
+ public static void main(String[] args) {
+	 float[][] input = {{0,0} , {0,1}, {1,0} ,{1,1}};
+	 float[] output = {0,0,0,1};
 
+	 Perceptron perceptron = new Perceptron(input, output);
+	 perceptron.train(0.01f, 200);
+	 
+	 System.out.println("There is error rate 0 our neural networ is ready");
+	 
+	 System.out.println(perceptron.calculateTheOutput(new float[]{0,0}));
+	 System.out.println(perceptron.calculateTheOutput(new float[]{0,1}));
+	 System.out.println(perceptron.calculateTheOutput(new float[]{1,0}));
+	 System.out.println(perceptron.calculateTheOutput(new float[]{1,1}));
+
+
+
+
+}
+ 
 }
