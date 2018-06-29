ea3c2be884ffa6bc14afba81bfc1d748a30410a3
Avinash Tiwari <qwe123kids@gmail.com>
2018-06-28 19:30:10 -0600

    no message

--- Java/EclpiseAINeuralNetwork/BackPropgarationNeuralNetwork/src/avinash/learn/backprogation/network/BackPropgationNeuralNetwork.java
+++ Java/EclpiseAINeuralNetwork/BackPropgarationNeuralNetwork/src/avinash/learn/backprogation/network/BackPropgationNeuralNetwork.java
@@ -2,9 +2,41 @@ package avinash.learn.backprogation.network;
 
 public class BackPropgationNeuralNetwork {
 	
-	public static final float LEARNING_RATE = 0.3f;
-	public static final float MOMENTUM = 0.6f;
-	public static final int ITERATION  = 100000;
+	private Layer[] layers;
+	
+	public BackPropgationNeuralNetwork(int inputSize, int hiddenSize, int outputSize){
+		layers = new Layer[2];
+		layers[0] = new Layer(inputSize,hiddenSize);
+		layers[1] = new Layer(inputSize,hiddenSize);
 
+	}
+	
+	public Layer getLayer(int index){
+		return layers[index];
+	}
 
+	public float[] run(float[] input){
+		
+		float[]  activation  = input;
+		for(int i =0; i < layers.length; i++){
+			activation = layers[i].run(activation);
+		}
+		
+		return activation;
+	}
+	
+	public void train(float[] input ,float targetOuput , float learningRate , float momemtum){
+		float[] calculatedOutput = run(input);
+		float[] error = new float[calculatedOutput.length];
+		
+		for(int i=0; i < error.length; i++){
+			error[i]= targetOuput[i] - calculatedOutput[i];
+		}
+		
+		for(int i=0; i < layers.length; i--){
+			error[i]= layers[i].train(error, learningRate, momemtum);
+
+		}
+
+	}
 }
--- Java/EclpiseAINeuralNetwork/BackPropgarationNeuralNetwork/src/avinash/learn/backprogation/network/NeuralNetConstants.java
+++ Java/EclpiseAINeuralNetwork/BackPropgarationNeuralNetwork/src/avinash/learn/backprogation/network/NeuralNetConstants.java
@@ -1,5 +1,8 @@
 package avinash.learn.backprogation.network;
 
 public class NeuralNetConstants {
+	public static final float LEARNING_RATE = 0.3f;
+	public static final float MOMENTUM = 0.6f;
+	public static final int ITERATION  = 100000;
 
 }
