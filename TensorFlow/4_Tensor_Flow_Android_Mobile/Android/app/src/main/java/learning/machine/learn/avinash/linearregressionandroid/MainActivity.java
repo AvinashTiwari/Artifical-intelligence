ac79e6fbf6c9393f7fcf6589892fc38be1069c37
Avinash Tiwari <qwe123kids@gmail.com>
2018-07-03 22:31:34 -0600

    no message

--- TensorFlow/4_Tensor_Flow_Android_Mobile/Android/app/src/main/java/learning/machine/learn/avinash/linearregressionandroid/MainActivity.java
+++ TensorFlow/4_Tensor_Flow_Android_Mobile/Android/app/src/main/java/learning/machine/learn/avinash/linearregressionandroid/MainActivity.java
@@ -2,12 +2,37 @@ package learning.machine.learn.avinash.linearregressionandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("tensorflow_inference");
+    }
+
+    private static final String MODEL_NAME ="file:///android_assets/optimized_frozen_linear_regression.pb";
+    private static final String INPUT_NODE ="x";
+    private static final String OUTPUT_NODE ="y_output";
+    private static final int[] INPUTSHAPE_NODE ={1,1};
+    private TensorFlowInferenceInterface InferenceInterface;
+
+    EditText edittext;
+    TextView textview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edittext = (EditText)findViewById(R.id.edit_text);
+        textview = (TextView) findViewById(R.id.text_view);
+    }
+
+    public void pressButton(View view){
+
     }
 }
