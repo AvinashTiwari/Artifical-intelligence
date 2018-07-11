4fa3f68211ac96c5a8ea93c66947937887526601
Avinash Tiwari <qwe123kids@gmail.com>
2018-07-10 22:31:50 -0600

    no message

---
+++ TensorFlow/5_Tensor_Flow_Android_Mobile_Mnist/AndroidApp/app/src/main/java/learning/machine/learn/avinash/simple_mnist_android/MainActivity.java
@@ -0,0 +1,13 @@
+package learning.machine.learn.avinash.simple_mnist_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
