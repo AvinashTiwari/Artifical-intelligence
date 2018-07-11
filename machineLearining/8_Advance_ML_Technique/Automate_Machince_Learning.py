f303e16509fb0a308e057e80c788a6f0e0c43c76
Avinash Tiwari <qwe123kids@gmail.com>
2018-07-10 22:34:11 -0600

    no message

--- machineLearining/8_Advance_ML_Technique/Automate_Machince_Learning.py
+++ machineLearining/8_Advance_ML_Technique/Automate_Machince_Learning.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 import pandas as pd
@@ -16,7 +16,7 @@ url = "pima_indians_diabetes.csv"
 names = ['preg', 'plas','pres','skin','test','mass','pedi', 'age','class']
 
 
-# In[15]:
+# In[2]:
 
 
 dataframe = pd.read_csv(url , names=names)
@@ -31,11 +31,53 @@ model =  Pipeline(estimators)
 seed = 7
 
 
-# In[17]:
+# In[3]:
 
 
 kfold = KFold(n_splits=10, random_state=seed)
 result = cross_val_score(model,X,y, cv=kfold)
 print(result)
 print(result.mean())
+
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import FeatureUnion
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest
+
+url = "pima_indians_diabetes.csv"
+names = ['preg', 'plas','pres','skin','test','mass','pedi', 'age','class']
+
+dataframe = pd.read_csv(url , names=names)
+array = dataframe.values
+X = array[:,0:8]
+y = array[:,8]
+
+
+
+# In[8]:
+
+
+feature =[]
+feature.append(('pca', PCA(n_components=3)))
+feature.append(('select_kbest', SelectKBest(k=6)))
+feature_union = FeatureUnion(feature)
+estimators = []
+estimators.append(('feature_union', feature_union))
+estimators.append(('logistic', LogisticRegression()))
+model = Pipeline(estimators)
+
+seed =7
+kfold = KFold(n_splits=10, random_state=seed)
+results = cross_val_score(model,X, y,cv=kfold)
+print(results)
+print(results.mean())
 
