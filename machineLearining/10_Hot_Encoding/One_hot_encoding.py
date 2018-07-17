59cc5bd8b48ba795eba485f621a8fd09b95a248e
Avinash Tiwari <qwe123kids@gmail.com>
2018-07-16 22:36:08 -0600

    no message

--- machineLearining/10_Hot_Encoding/One_hot_encoding.py
+++ machineLearining/10_Hot_Encoding/One_hot_encoding.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 from  numpy import array
@@ -10,20 +10,20 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
 
-# In[4]:
+# In[2]:
 
 
 dataset = ['Pizza', 'Burger', 'Beacon','Beacon','Beacon','Burger','Pizza','Burger']
 
 
-# In[5]:
+# In[3]:
 
 
 values = array(dataset)
 print(values)
 
 
-# In[6]:
+# In[4]:
 
 
 labelEncoder  = LabelEncoder()
@@ -31,7 +31,7 @@ interger_encoder = labelEncoder.fit_transform(values)
 print(interger_encoder)
 
 
-# In[7]:
+# In[5]:
 
 
 oneHot = OneHotEncoder(sparse=False)
@@ -40,9 +40,33 @@ onehot_endcoded = oneHot.fit_transform(interger_encoded)
 print(onehot_endcoded)
 
 
-# In[10]:
+# In[6]:
 
 
 inverted_result = labelEncoder.inverse_transform([argmax(onehot_endcoded[0,:])])
 print(inverted_result)
+
+
+# In[7]:
+
+
+from keras.utils import to_categorical
+dataset2 = ['Pizza', 'Burger', 'Beacon','Beacon','Beacon','Burger','Pizza','Burger']
+myarray = array(dataset2)
+print(myarray)
+
+
+# In[8]:
+
+
+interger_encoder = labelEncoder.fit_transform(myarray)
+keras_encoded = to_categorical(interger_encoder)
+print(keras_encoded)
+
+
+# In[10]:
+
+
+inverted_result2= argmax(keras_encoded[0])
+print(inverted_result2)
 
