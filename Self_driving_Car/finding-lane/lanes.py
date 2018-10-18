f1427dd1f61ce6a8061268d15c6df96563ca9b33
Avinash Tiwari <qwe123kids@gmail.com>
2018-10-18 17:55:35 -0600

    no message

--- Self_driving_Car/finding-lane/lanes.py
+++ Self_driving_Car/finding-lane/lanes.py
@@ -1,9 +1,17 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5),0)
+    canny = cv2.Canny(blur, 5,150)
+    return canny
+
 
 image = cv2.imread('test_image.jpg')
 lang_image = np.copy(image)
-gray = cv2.cvtColor(lang_image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(gray, (5,5),0)
-cv2.imshow('result',blur)
+canny = canny(lang_image)
+##cv2.imshow('result',canny)
+plt.imshow(canny)
 cv2.waitKey(0)
