410bd07545fd3fe5ce888b389726995c0d7b16d1
Avinash Tiwari <qwe123kids@gmail.com>
2018-10-18 17:24:44 -0600

    no message

--- Self_driving_Car/finding-lane/lanes.py
+++ Self_driving_Car/finding-lane/lanes.py
@@ -1,5 +1,9 @@
 import cv2
+import numpy as np
 
 image = cv2.imread('test_image.jpg')
-cv2.imshow('result',image)
+lang_image = np.copy(image)
+gray = cv2.cvtColor(lang_image, cv2.COLOR_RGB2GRAY)
+blur = cv2.GaussianBlur(gray, (5,5),0)
+cv2.imshow('result',blur)
 cv2.waitKey(0)
