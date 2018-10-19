95636456d2455d3cf829d86234f2ef9274818220
Avinash Tiwari <qwe123kids@gmail.com>
2018-10-19 12:58:31 -0600

    no message

--- Self_driving_Car/finding-lane/lanes.py
+++ Self_driving_Car/finding-lane/lanes.py
@@ -8,10 +8,18 @@ def canny(image):
     canny = cv2.Canny(blur, 5,150)
     return canny
 
+def region_of_interset(image):
+    height = image.shape[0]
+    polygon = np.array([[(200,height),(1100, height), (550,250)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask,polygon,255)
+    return mask;
+
 
 image = cv2.imread('test_image.jpg')
 lang_image = np.copy(image)
 canny = canny(lang_image)
-##cv2.imshow('result',canny)
-plt.imshow(canny)
+cv2.imshow('result',region_of_interset(canny))
+#plt.imshow(canny)
+#plt.show()
 cv2.waitKey(0)
