22c2f5d6e878760fa8ccc186818c01b7bac2daa0
Avinash Tiwari <qwe123kids@gmail.com>
2018-10-20 17:48:29 -0600

    no message

--- Self_driving_Car/finding-lane/lanes.py
+++ Self_driving_Car/finding-lane/lanes.py
@@ -8,6 +8,15 @@ def canny(image):
     canny = cv2.Canny(blur, 5,150)
     return canny
 
+def display_lines(images,lines):
+    line_image = np.zeros_like(images)
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2 = line.reshape(4)
+            cv2.line(line_image,(x1,y1), (x2,y2),(255,0,0),10)
+    return  line_image
+
+
 def region_of_interset(image):
     height = image.shape[0]
     polygon = np.array([[(200,height),(1100, height), (550,250)]])
@@ -21,7 +30,10 @@ image = cv2.imread('test_image.jpg')
 lang_image = np.copy(image)
 canny = canny(lang_image)
 cropped_image = region_of_interset(canny)
-cv2.imshow('result',cropped_image)
+lines = cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40, maxLineGap=5)
+line_image = display_lines(lang_image,lines)
+combo_image = cv2.addWeighted(lang_image, 0.8, line_image, 1, 1)
+cv2.imshow('result',combo_image)
 #plt.imshow(canny)
 #plt.show()
 cv2.waitKey(0)
