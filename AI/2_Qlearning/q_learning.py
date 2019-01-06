a134330afc4ea167a5b5d21f1f1ebfba9a1e2f80
avinash\avinash.t <qwe123kids@gmail.om>
2019-01-05 20:53:19 -0700

    Q learning Start

---
+++ AI/2_Qlearning/q_learning.py
@@ -0,0 +1,17 @@
+import numpy as np
+gamma = 0.75
+alpha = 0.9
+
+location_to_state = {'A' : 0,
+                     'B' : 1,
+                     'C' : 2,
+                     'D' : 3,
+                     'E' : 4,
+                     'F' : 5,
+                     'G' : 6,
+                     'H' : 7,
+                     'I' : 8,
+                     'J' : 9,
+                     'K' : 10,
+                     'L' : 11}
+actions = [0,1,2,3,4,5,6,7,8,9,10,11]
