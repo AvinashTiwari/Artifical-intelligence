503ac021445d55e3e1cb6145cf4c25f056d82372
avinash\avinash.t <qwe123kids@gmail.om>
2019-01-07 19:32:48 -0700

    AI Q

--- AI/2_Qlearning/q_learning.py
+++ AI/2_Qlearning/q_learning.py
@@ -31,3 +31,14 @@ R = np.array([[0,1,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,1,0,0,1,0]])
 
 Q = np.array(np.zeros([12,12]))
+
+for i in range(1000):
+    current_state = np.random.randint(0,12)
+    playable_actions = []
+    for j in range(12):
+        if R[current_state, j] > 0:
+            playable_actions.append(j)
+    next_state = np.random.choice(playable_actions)
+    TD = R[current_state, next_state] + gamma*Q[next_state, np.argmax(Q[next_state,])]- Q[current_state, next_state]
+    Q[current_state, next_state] = Q[current_state, next_state] + alpha*TD
+            
