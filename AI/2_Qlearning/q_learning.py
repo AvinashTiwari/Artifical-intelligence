1f63f19930b3f03e457c5ae38d521472be78e951
avinash\avinash.t <qwe123kids@gmail.om>
2019-01-11 19:02:06 -0700

    decision trr intro

--- AI/2_Qlearning/q_learning.py
+++ AI/2_Qlearning/q_learning.py
@@ -23,7 +23,7 @@ R = np.array([[0,1,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,1,0,0,0,0],
               [0,0,0,0,0,0,0,0,1,0,0,0],
               [0,1,0,0,0,0,0,0,0,1,0,0],
-              [0,0,1,0,0,0,1,1,0,0,0,0],
+              [0,0,1,0,0,0,1000,1,0,0,0,0],
               [0,0,0,1,0,0,1,0,0,0,0,1],
               [0,0,0,0,1,0,0,0,0,1,0,0],
               [0,0,0,0,0,1,0,0,1,0,1,0],
@@ -41,4 +41,14 @@ for i in range(1000):
     next_state = np.random.choice(playable_actions)
     TD = R[current_state, next_state] + gamma*Q[next_state, np.argmax(Q[next_state,])]- Q[current_state, next_state]
     Q[current_state, next_state] = Q[current_state, next_state] + alpha*TD
-            
+
+def route(starting_location, ending_location):
+    route = [starting_location]
+    next_location = starting_location
+    while (next_location != ending_location):
+        starting_state = location_to_state[starting_location]
+        next_state = np.argmax(Q[starting_state,])
+        next_location = state_to_location[next_state]
+        route.append(next_location)
+        starting_location = next_location
+    return route
