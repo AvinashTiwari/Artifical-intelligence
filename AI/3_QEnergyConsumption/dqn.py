d25bd6228bd8dfcd2764113528ded010c3c11004
avinash\avinash.t <qwe123kids@gmail.om>
2019-02-05 18:06:38 -0700

    Ai

--- AI/3_QEnergyConsumption/dqn.py
+++ AI/3_QEnergyConsumption/dqn.py
@@ -6,3 +6,17 @@ Created on Mon Feb  4 14:38:57 2019
 """
 
 import numpy as np
+
+class DQN(object):
+    
+    def __init__(self, max_memory = 100, discount = 0.9):
+        self.memory = list()
+        self.max_memory = max_memory
+        self.discount = discount
+    
+    def remember(self, transition, game_over):
+        self.memory.append([transition, game_over])
+        if len(self.memory) > self.max_memory:
+            del self.memory[0]
+            
+        
