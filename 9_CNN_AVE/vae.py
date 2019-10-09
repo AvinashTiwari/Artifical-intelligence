f72caeaa75c47ae7c4aeee94b856aa32b0926532
AvinashTiwari85 <qwe123kids@gmail.com>
2019-10-08 22:26:22 -0600

    no message

--- 9_CNN_AVE/vae.py
+++ 9_CNN_AVE/vae.py
@@ -1,2 +1,23 @@
 import numpy as np
 import tensorflow as tf
+
+class ConVAE(object):
+    
+    def __init__(self, z_size=32, batch_size=1, learning_rate=0.0001, kl_tolerance=0.5, is_training=False, reuse=False, gpu_mode=False ):
+        self.z_size = z_size
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.kl_tolerance = kl_tolerance
+        self.is_training = is_training
+        self.reuse = reuse
+        with tf.variable_scope('con_vae',reuse=self.reuse):
+            if not gpu_mode:
+                with tf.device('/cpu:0'):
+                    tf.logging.info('Model using cpu')
+                    self._build_graph()
+            else:
+                tf.logging.info('Model using gpu')
+                self._build_graph()
+        
+        self._init_session()        
+        
