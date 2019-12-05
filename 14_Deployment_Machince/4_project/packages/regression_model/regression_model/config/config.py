42c7ffbc8d618f46ae1611622b0f441c439bc30f
AvinashTiwari85 <qwe123kids@gmail.com>
2019-12-04 19:46:11 -0700

    no message

--- 14_Deployment_Machince/4_project/packages/regression_model/regression_model/config/config.py
+++ 14_Deployment_Machince/4_project/packages/regression_model/regression_model/config/config.py
@@ -4,10 +4,13 @@ import regression_model
 
 
 PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
+
 TRAINED_MODEL_DIR = PACKAGE_ROOT / 'trained_models'
 DATASET_DIR = PACKAGE_ROOT / 'datasets'
+print(TRAINED_MODEL_DIR)
+print(DATASET_DIR)
 
-# data
+
 TESTING_DATA_FILE = 'test.csv'
 TRAINING_DATA_FILE = 'train.csv'
 TARGET = 'SalePrice'
